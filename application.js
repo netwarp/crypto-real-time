e2e66bff6f34194e8f0da9cb94644e8a18108fcd
JeremyBellaiche <contact@jeremybellaiche.com>
2017-12-31 20:12:30 +0100

    refresher

--- application.js
+++ application.js
@@ -13,29 +13,16 @@ var real_time_refresher = new Vue({
 		refresher() {
 			var url = 'https://api.coinmarketcap.com/v1/ticker/?limit=10'
 
-			fetch(url)
-			.then((response) => {
-				return response.json();
-			})
-			.then((data) => {
-				this.currencies = data
-				console.log(data)
-			})
-		},
-
-		formatDate(timestamp) {
-			var d = new Date(timestamp * 1000)
-
-			var year = parseInt(d.getFullYear())
-			var month  = parseInt(d.getMonth())
-			var day = parseInt(d.getDate())
-
-			var hour = parseInt(d.getHours())
-			var minute = parseInt(d.getMinutes())
-			var seconds = parseInt(d.getSeconds())
-
-			var string = `${day}`
-			return string
+			setInterval(() => {
+				fetch(url)
+				.then((response) => {
+					return response.json();
+				})
+				.then((data) => {
+					this.currencies = data
+					console.log(data)
+				})
+			}, 180000)
 		}
-	},
+	}
 })
